b79c0e03cd2c97f0a606dcec711a1da817b943a7
dltv226 <dltv226@gmail.com>
2024-11-16 02:14:49 +0700

    update url for render

--- src/utilities/axiousEdition.ts
+++ src/utilities/axiousEdition.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://uav-store-backend.onrender.com/api/v1',
   withCredentials: true, // Always send cookies with requests
 });
 
